973fbffd653d5a7d4ac6f5537def303c9a80aaf6
DevCHW <ggoma003@naver.com>
2024-10-14 00:24:02 +0900

    LazyConnectionDataSourceProxy 예제 프로젝트 생성

---
+++ spring-lazyconnectiondatasourceproxy/src/main/java/com/study/lazyconnectiondatasourceproxy/LazyconnectiondatasourceproxyApplication.java
@@ -0,0 +1,13 @@
+package com.study.lazyconnectiondatasourceproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazyconnectiondatasourceproxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazyconnectiondatasourceproxyApplication.class, args);
+	}
+
+}
---
+++ spring-lazyconnectiondatasourceproxy/src/test/java/com/study/lazyconnectiondatasourceproxy/LazyconnectiondatasourceproxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.lazyconnectiondatasourceproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazyconnectiondatasourceproxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
