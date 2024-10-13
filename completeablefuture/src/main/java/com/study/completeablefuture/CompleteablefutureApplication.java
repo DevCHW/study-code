d9da8121330094b53fd8ca9add6dfa5905a9f865
DevCHW <ggoma003@naver.com>
2024-10-13 09:15:52 +0900

    CompleteableFuture 튜토리얼 시작

---
+++ completeablefuture/src/main/java/com/study/completeablefuture/CompleteablefutureApplication.java
@@ -0,0 +1,13 @@
+package com.study.completeablefuture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompleteablefutureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompleteablefutureApplication.class, args);
+	}
+
+}
---
+++ completeablefuture/src/test/java/com/study/completeablefuture/CompleteablefutureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.completeablefuture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompleteablefutureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
