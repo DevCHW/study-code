86f829e816678bf220b09f11dc8ea4499b3880b0
DevCHW <ggoma003@naver.com>
2025-06-09 19:16:10 +0900

    Rate Limiter 프로젝트 추가

---
+++ rate-limiter/src/main/kotlin/io/devchw/ratelimiter/RateLimiterApplication.kt
@@ -0,0 +1,11 @@
+package io.devchw.ratelimiter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RateLimiterApplication
+
+fun main(args: Array<String>) {
+    runApplication<RateLimiterApplication>(*args)
+}
---
+++ spring-sse/src/main/kotlin/io/devchw/springsse/NotificationEntity.kt
@@ -0,0 +1,8 @@
+package io.devchw.springsse
+
+/**
+ * @author DevCHW
+ * @since 2025-05-02
+ */
+class NotificationEntity {
+}
