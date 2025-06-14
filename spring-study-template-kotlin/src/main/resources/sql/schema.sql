091bcdb72312bc5b1d99b27712fd18a7efb9bae4
DevCHW <ggoma003@naver.com>
2025-06-14 14:16:10 +0900

    schema.sql Auto Increment 속성 추가

--- spring-study-template-kotlin/src/main/resources/sql/schema.sql
+++ spring-study-template-kotlin/src/main/resources/sql/schema.sql
@@ -4,7 +4,7 @@ drop table if exists example_users cascade;
 create table example_users
 (
   id         varchar(13)  not null comment 'PK'
-    primary key,
+    primary key auto_increment,
   username   varchar(255) not null comment '이름',
   created_at timestamp(6) not null comment '생성 시점',
   updated_at timestamp(6) not null comment '마지막 수정 시점'
