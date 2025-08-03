c8b3122c791f116896bee43dd85ee77fd83c8690
DevCHW <ggoma003@naver.com>
2025-08-04 02:58:33 +0900

    chore: Add Pull Request Template, Auto Assign Config File

---
+++ spring-batch/src/test/kotlin/com/abear/springbatch/SpringBatchApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.abear.springbatch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBatchApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
